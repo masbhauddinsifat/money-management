a8edc11e37b60dc484b3f55d603752d6b998f304
Masbha Uddin <masbhauddinsifat@gmail.com>
2021-12-29 00:38:10 +0600

    helmet add

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.enableCors();
   app.setGlobalPrefix('api/v1');
 
